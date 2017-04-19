ce8832c9570d068cf3794e3b278a69a1de876edd
satomikimura <satomi.kimura08@gmail.com>
2017-04-19 05:17:31 +0000

    set preparation2

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,5 +1,6 @@
 Rails.application.config.generators do |g|
     g.stylesheets false
     g.javascripts false
-    g.helper falseg.skip_routes true
+    g.helper false
+    falseg.skip_routes true
 end
